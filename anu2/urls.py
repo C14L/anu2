e2c904805901d3e547d5f67addc55e8ef6606beb
Christian Steimel <me@C14L.com>
2015-08-23 12:14:51 +0200

    Move dtrcity routes into dtrcity app.

--- anu2/urls.py
+++ anu2/urls.py
@@ -24,33 +24,25 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from anuncios import views
-from dtrcity import views as city_views
+import dtrcity.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dtrcity/', include(dtrcity.urls)),
 
-    # dtrcity API.
-    # -- TODO: Make it a real module with its own urls.py file!
-
-    url(r'^dtrcity/api/v1/all-countries.json$',
-        city_views.all_countries, name='all_countries'),
-    url(r'^dtrcity/api/v1/autocomplete-crc.json$',
-        city_views.city_autocomplete_crc, name='city_autocomplete_crc'),
-    url(r'^dtrcity/api/v1/cities-in-country.json$',
-        city_views.cities_in_country, name='cities_in_country'),
-    url(r'^dtrcity/api/v1/city-by-latlng.json$',
-        city_views.city_by_latlng, name='city_by_latlng'),
-
-
-    url(r'^api/v1/users/$', views.UserListAPIView.as_view(), name="user-item"),
-    url(r'^api/v1/users/(?P<pk>[0-9]+)$', views.UserItemAPIView.as_view(), name="user-list"),
-
-    url(r'^api/v1/posts/$', views.PostListAPIView.as_view(), name="post-list"),
-    url(r'^api/v1/posts/(?P<pk>[0-9]+)$', views.PostItemAPIView.as_view(), name="post-item"),
-
-    url(r'^api/v1/categories/$', views.CategoriesListAPIView.as_view(), name="categories-list"),
-    url(r'^api/v1/categories/(?P<pk>[a-z0-9-]+)$', views.CategoriesItemAPIView.as_view(), name="categories-item"),
-
+    url(r'^api/v1/users/$', 
+        views.UserListAPIView.as_view(), name="user-item"),
+    url(r'^api/v1/users/(?P<pk>[0-9]+)$', 
+        views.UserItemAPIView.as_view(), name="user-list"),
+    url(r'^api/v1/posts/$', 
+        views.PostListAPIView.as_view(), name="post-list"),
+    url(r'^api/v1/posts/(?P<pk>[0-9]+)$', 
+        views.PostItemAPIView.as_view(), name="post-item"),
+    url(r'^api/v1/categories/$', 
+        views.CategoriesListAPIView.as_view(), name="categories-list"),
+    url(r'^api/v1/categories/(?P<pk>[a-z0-9-]+)$', 
+        views.CategoriesItemAPIView.as_view(), name="categories-item"),
+    
     url(r'^.*$', views.AppHTMLView.as_view(), name="app"),
 ]
 
